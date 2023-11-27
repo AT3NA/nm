2bf68bfbdd4e850672b202820f514f51b2517cd8
HARAJIT <110291590+HARAJIT05@users.noreply.github.com>
2023-11-27 14:29:39 +0530

    Update EULER.c

--- EULER.c
+++ EULER.c
@@ -12,20 +12,21 @@ main()
     printf("\n Enter the value of x0,y0,xn,h:");
     scanf("%f%f%f%f", &x0, &y0, &xn, &h);
     do {
-        printf("\n The value of y(%f)=%f", x0, y0);
         y1 = y0 + h * f(x0, y0);
         x0 = x0 + h;
         y0 = y1;
+        printf("\n The value of y(%f)=%f", x0, y0);
     } while (x0 <= xn);
 }
 
 
-//Enter the value of x0,y0,xn,h:0
+
+
+// Enter the value of x0,y0,xn,h:0
 //1
 //0.8
 //0.1
 
- //The value of y(0.000000)=1.000000
  //The value of y(0.100000)=1.100000
  //The value of y(0.200000)=1.220000
  //The value of y(0.300000)=1.362000
@@ -33,3 +34,4 @@ main()
  //The value of y(0.500000)=1.721020
  //The value of y(0.600000)=1.943122
  //The value of y(0.700000)=2.197434
+ //The value of y(0.800000)=2.487178[1]
